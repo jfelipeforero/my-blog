a9b0d6657d21d0a4cabf3712e9a4cecaa4be0a46
jfelipeforero <felipe__2001@hotmail.com>
2023-07-26 16:08:42 +0200

    Contact page added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 import { AppComponent } from './app.component';
+import { ContactModule } from './contact/contact.module';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { AppComponent } from './app.component';
     AppRoutingModule,
     CoreModule,
     SharedModule,
+    ContactModule
   ],
   providers: [],
   bootstrap: [AppComponent]
