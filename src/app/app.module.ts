e3d8d5c34cc7423e8c30437de83a32f1244a5672
jfelipeforero <felipe__2001@hotmail.com>
2023-07-28 09:25:03 -0500

    Add scully SSG for blogs route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 import { AppComponent } from './app.component';
 import { ContactModule } from './contact/contact.module';
+import { ScullyLibModule } from "@scullyio/ng-lib";
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { ContactModule } from './contact/contact.module';
     AppRoutingModule,
     CoreModule,
     SharedModule,
-    ContactModule
+    ContactModule,
+    ScullyLibModule
   ],
   providers: [],
   bootstrap: [AppComponent]
